ed2d8f8c4b9253d6f39d58a5c3843e9c4213ec4a
PeterArce96 <parce0496@gmail.com>
2022-08-26 12:19:38 -0500

    consola, algunos ejemplos de console

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,10 +1,28 @@
-// Pregunta al usuario su nombre
+// // Pregunta al usuario su nombre
 const nombre = prompt('Cual es tu nombre?');
-// toma el nombre y lo muestra en pantalla
+// // toma el nombre y lo muestra en pantalla
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo Javascript Moderno`;
 
-// Asigna el valor hacia la variable producto
+// // Asigna el valor hacia la variable producto
 const producto = 'Monitor 24 pulgadas';
 /*
 Comentar varias líneas de codigo
 */
+console.warn('ADVERTENCIA');
+
+// Limpiar la consola
+console.clear();
+
+console.time('Hola');
+
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+
+// Tiempo que demoró en ejecutar el codigo
+console.timeEnd('Hola');
