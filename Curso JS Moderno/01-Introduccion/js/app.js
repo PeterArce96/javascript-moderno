289a2bd2e178fa585d814e5344a8e84935fa4b83
PeterArce96 <parce0496@gmail.com>
2022-08-26 12:10:06 -0500

    comentarios

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,2 +1,10 @@
+// Pregunta al usuario su nombre
 const nombre = prompt('Cual es tu nombre?');
+// toma el nombre y lo muestra en pantalla
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo Javascript Moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
+/*
+Comentar varias líneas de codigo
+*/
