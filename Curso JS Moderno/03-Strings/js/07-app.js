589b7b8ac45078ae57a3e9c25177c17de9702791
PeterArce96 <parce0496@gmail.com>
2022-08-26 22:59:57 -0500

    Strings - toLowerCase, toUpperCase, toString

---
+++ Curso JS Moderno/03-Strings/js/07-app.js
@@ -0,0 +1,18 @@
+// Strings - toLowerCase, toUpperCase
+const producto = 'Monitor 20 Pulgadas';
+
+// toUpperCase() --> Convierte el string a Mayúsculas
+console.log(producto.toUpperCase());
+
+// toLowerCase() --> Convierte el string a Minúsuculas
+console.log(producto.toLowerCase());
+
+// Para proyectos
+const email = "CORREO@CORREO.COM"
+console.log(email.toLowerCase());
+
+const precio = 300;
+// Si ves el resultado de consola en color azul, es tipo Number
+console.log(precio);
+// toString() --> Para convertir a String
+console.log(precio.toString());
