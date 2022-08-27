591a8e682ef4b185bf9b90ebf56b6a9b22905bf1
PeterArce96 <parce0496@gmail.com>
2022-08-26 21:07:08 -0500

    Strings - crear Strings

---
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,10 @@
+// STRINGS --> TEXTO
+// 1° forma de declarar
+const producto = "Monitor de 20 pulgadas";
+// 2° forma de declarar
+const producto2 = String('Monitor 24 pulgadas');
+// 3° forma de declarar
+const producto3 = new String('Monitor de 27 pulgadas');
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
