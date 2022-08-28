59f89f72ab7ebcc87f4a28a4025112aebf4f0272
PeterArce96 <parce0496@gmail.com>
2022-08-27 21:29:06 -0500

    Numbers - coperaciones

---
+++ Curso JS Moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,18 @@
+// NUMBERS - operaciones
+const numero1 = 30;
+const numero2 = 20;
+
+let resultado;
+
+// suma
+resultado = numero1 + numero2;
+// resta
+resultado = numero1 - numero2;
+// multiplicación
+resultado = numero1 * numero2;
+// división
+resultado = numero1 / numero2;
+// Módulo - residuo
+resultado = numero1 % numero2;
+
+console.log(resultado);
