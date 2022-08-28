b7eb376591d37cd27f37d04eb857c8a58263b158
PeterArce96 <parce0496@gmail.com>
2022-08-27 22:02:17 -0500

    Numbers - incrementos y decrementos

---
+++ Curso JS Moderno/04-Numeros/js/05-app.js
@@ -0,0 +1,29 @@
+// Numbers - incrementos y decrementos
+let puntaje = 5;
+
+// Incremento ++ --> Imprime y luego aumenta en 1;
+console.log(puntaje++); // 5
+console.log(puntaje); // 6
+
+// ++ Incremento --> Aumenta en 1 y luego imprime;
+console.log(++puntaje); // 7
+
+
+let puntaje2 = 5;
+// Decremento -- --> Imprime y luego disminuye en 1;
+console.log(puntaje2--);//5
+console.log(puntaje2);//4
+
+// -- Decremento --> Disminuye en 1 y luego imprime;
+console.log(--puntaje2);//3
+
+// Aumentar en mayor cantidad
+let puntaje3 = 5;
+
+// puntaje3 = puntaje3 + 3;
+puntaje3 += 3;
+console.log(puntaje3); // 5 + 3 = 8
+
+// Disminuir en mayor cantidad
+puntaje3 -= 4; // 8 - 4 = 4
+console.log(puntaje3);
