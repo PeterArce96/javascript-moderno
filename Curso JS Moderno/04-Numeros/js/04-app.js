f7570413159a36bd5a9a820d04520d1d2cbdf34f
PeterArce96 <parce0496@gmail.com>
2022-08-27 21:48:30 -0500

    Numbers - Orden de operaciones

---
+++ Curso JS Moderno/04-Numeros/js/04-app.js
@@ -0,0 +1,14 @@
+// Numbers - Orden de operaciones
+let resultado;
+
+// Se realiza primero la multiplicación
+resultado = 20 + 30 * 2;
+// Se realiza primero lo que está dentro del paréntesis
+resultado = (20 + 30) * 2;
+
+// 20% de descuento en tu carrito de compras
+resultado = (20+30+40+50+60) * 0.2;
+// Impuesto de 16%;
+resultado = (20 + 30) * 1.16;
+
+console.log(resultado);
