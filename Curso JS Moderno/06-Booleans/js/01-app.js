37fef1bd38e01c7f1293fc78ee30259e2dcd99dd
PeterArce96 <parce0496@gmail.com>
2022-08-31 22:01:46 -0500

    Booleans, comparar booleans

---
+++ Curso JS Moderno/06-Booleans/js/01-app.js
@@ -0,0 +1,16 @@
+// Booleans - creaqr y comparar booleans
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+
+// console.log(boolean1);
+// console.log(boolean2);
+
+// console.log(typeof boolean1);
+
+// Colocarlo en un string no asigna un valor similar como con los números o un string, así se comparen con igualdad abstracta (==);
+console.log(boolean1 == boolean3);
+
+const boolean4 = new Boolean(true);
+console.log(boolean4);
+console.log(typeof boolean4);
---
+++ Curso JS Moderno/06-Booleans/js/02-app.js
@@ -0,0 +1,8 @@
+// Boolean - comparar segunda parte
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1 === boolean2); //false
+
+console.log(boolean1 === true); //true
+console.log(boolean1 == "true"); //false
