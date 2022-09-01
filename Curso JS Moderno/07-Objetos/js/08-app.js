08195389ec1fecaf704cb8d2de45fd51061bb683
PeterArce96 <parce0496@gmail.com>
2022-08-31 23:18:24 -0500

    Objetos - congelar un objeto

---
+++ Curso JS Moderno/07-Objetos/js/08-app.js
@@ -0,0 +1,22 @@
+// OBJETOS - Congelar un objeto para no poder modificarlo
+
+// Mediante esta sentencia, no se permite las malas prácticas en JS
+"use strict";
+
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+}
+// La siguiente sentencia se activa con el "use strict", congela el objeto
+Object.freeze( producto );
+
+// Provocan error
+// producto.disponible = false;
+// producto.imagen = "imagen.jpg";
+// delete producto.precio;
+
+console.log(producto);
+// Como saber si un objeto está congelado y no se puede modificar
+console.log(Object.isFrozen(producto));
+
