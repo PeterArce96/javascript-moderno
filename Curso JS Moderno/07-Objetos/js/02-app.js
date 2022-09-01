c81db7f24efbb25269f46c03bf8b34815652604c
PeterArce96 <parce0496@gmail.com>
2022-08-31 22:18:30 -0500

    Objetos, como acceder a un objeto

---
+++ Curso JS Moderno/07-Objetos/js/02-app.js
@@ -0,0 +1,17 @@
+// Objetos, como acceder a un objeto
+
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto);
+
+// La sintaxis de punto -- MÁS COMÚN
+console.log(producto.nombre);
+console.log(producto.precio);
+console.log(producto.disponible);
+
+// Mediante corchetes
+console.log(producto['nombre']);
