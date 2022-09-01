521f0049cde2dc7fb7a2306054c312233019bed5
PeterArce96 <parce0496@gmail.com>
2022-08-31 22:25:30 -0500

    Objetos - agregar o eliminar propiedad de un objeto

---
+++ Curso JS Moderno/07-Objetos/js/03-app.js
@@ -0,0 +1,14 @@
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+    // imagen: "imagen.jpg",
+}
+
+// Agregar nuevas propiedades al objeto
+producto.imagen = "imagen.jpg";
+
+// Eliminar una propiedad al objeto
+delete producto.disponible;
+
+console.log(producto);
