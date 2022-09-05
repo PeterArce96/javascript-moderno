e9f522ef328916a9d36172382266553ea43c773d
PeterArce96 <parce0496@gmail.com>
2022-09-04 21:07:16 -0500

    Arrays - Recorrer un array

---
+++ Curso JS Moderno/08-Arrays/js/03-app.js
@@ -0,0 +1,12 @@
+// ARRAYS - Recorrer un array
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+console.table(meses);
+
+// .length --> Medida del arreglo
+console.log(meses.length);
+
+// Recorrer el array con FOR
+for(let i = 0; i < meses.length; i++ ) {
+    console.log(meses[i]);
+}
