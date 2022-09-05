e92d2eb1dea8b1d29fe5a35edcc4d8b7757cd97c
PeterArce96 <parce0496@gmail.com>
2022-09-04 21:11:10 -0500

    Arrays - agregar nuevo valores a un array, sin metodos

---
+++ Curso JS Moderno/08-Arrays/js/04-app.js
@@ -0,0 +1,10 @@
+// ARRAYS - Agregar nuevos valores a un array
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio'];
+
+// Reemplaza un elemento del arreglo así sea const
+meses[0] = "Nuevo mes"
+// Agregar un elemento al arreglo
+meses[10] = 'Ultimo mes'
+
+console.table(meses);
