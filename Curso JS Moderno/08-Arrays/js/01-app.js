db5790bd6549e0bae96bd3959f5904cb85aab50f
PeterArce96 <parce0496@gmail.com>
2022-09-04 20:55:05 -0500

    Arrays - Crecar un array

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,15 @@
+// ARRAYS - Crear un array
+
+// 1° Forma
+const numeros = [10, 20, 30];
+// 2° forma
+const meses = new Array('enero', 'Febrero', 'Marzo');
+
+// índice --> posición en el arreglo
+
+console.log(numeros);
+console.log(meses);
+
+// Arreglo que contiene datos de todo tipo
+const deTodo = ["Hola", 10, true, "si", null, {nombre: 'Juan', trabajo: 'Programador'}, [1,2,3]];
+console.log(deTodo);
