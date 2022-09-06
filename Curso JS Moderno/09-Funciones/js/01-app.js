cdba84b7f393fa2f93b538585ba1ef92fc792bc0
PeterArce96 <parce0496@gmail.com>
2022-09-05 19:17:27 -0500

    Funciones - creación, function expression, function declaration

---
+++ Curso JS Moderno/09-Funciones/js/01-app.js
@@ -0,0 +1,16 @@
+// Funciones - Crear una funcion, Function expression y function declaration
+
+// Las funciones son una serie de instrucciones o línea de código que realizan una acción, permiten un código más ordenado y facil de mantener, son reutilizables.
+
+// Function declaration
+function sumar() {
+    console.log(2 + 2);
+}
+// llamar a la función
+sumar(); //4
+
+// Function expression
+const sumar2 = function() {
+    console.log(3 + 3);
+}
+sumar2();//6
