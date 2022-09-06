063811722daec8b975271d035ac463213e2dfdb8
PeterArce96 <parce0496@gmail.com>
2022-09-05 20:15:59 -0500

    Funciones - ejemplo con m+últiples funciones que se pasan valores

---
+++ Curso JS Moderno/09-Funciones/js/08-app.js
@@ -0,0 +1,27 @@
+// Funciones - Ejemplo con múltiple funciones que se pasan valores
+
+function sumar(a,b) {
+    return a + b;
+}
+const resultado = sumar(2,3);
+console.log(resultado);
+
+// Ejemplo más avanzado
+let total = 0;
+function agregarCarrito(precio) {
+    return total += precio;
+}
+
+function calcularImpuesto(total) {
+    return total * 1.15;
+}
+
+agregarCarrito(300);
+agregarCarrito(100);
+agregarCarrito(600);
+
+const totalPagar = calcularImpuesto(total);
+
+console.log(`El total a pagar es de ${totalPagar}`);
+
+console.log(total);
