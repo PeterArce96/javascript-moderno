f2051249bf3b77a97696ba07242aecd3697e52e1
PeterArce96 <parce0496@gmail.com>
2022-09-13 21:55:39 -0500

    Estructuras - Mayor y Menor que

---
+++ Curso JS Moderno/10-Estructuras/js/03-app.js
@@ -0,0 +1,11 @@
+// Estructuras de control - Mayor y menor que
+
+const dinero = 300;
+const totalAPagar = 300;
+
+// Mayor o igual, porque si tenemos justo 300, si podemos pagar
+if (dinero >= totalAPagar) {
+    console.log("Si podemos pagar")
+} else {
+    console.log("Fondos insuficientes");
+}
