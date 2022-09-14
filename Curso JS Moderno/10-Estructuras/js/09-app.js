21fb5f8d17c6f83035efd7cd1c4438a244125838
PeterArce96 <parce0496@gmail.com>
2022-09-13 23:39:03 -0500

    Estructuras - Operador ternario

---
+++ Curso JS Moderno/10-Estructuras/js/09-app.js
@@ -0,0 +1,8 @@
+// Estructuras - Operador ternario
+
+const autenticado = true;
+const puedePagar = false;
+
+console.log(autenticado ? puedePagar ? 'Si está autenticado y puede pagar': 'Si autenticado, pero no puede pagar': 'No no está autenticado');
+
+// ternario anidado, como un if dentro de otro if
