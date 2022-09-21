6bc04223608585a50a7795f3513c39533b568808
PeterArce96 <parce0496@gmail.com>
2022-09-21 17:28:59 -0500

    EnviarEmail - Validar el email

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -20,7 +20,7 @@ function eventListeners(){
 
 
 
-// Funciones
+// FUNCIONES
 
 function iniciarApp() {
     // disabled hace que el boton no se pueda presionar
@@ -36,13 +36,20 @@ function validarFormulario(e) {
     }else {
         e.target.classList.add('border', 'border-red-500');
 
-        mostrarError();
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email') {
+        const resultado = e.target.value.indexOf('@');
+        if(resultado < 0){
+            mostrarError('El email no es válido');
+        }
     }
 }
 
-function mostrarError() {
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('border', 'border-red-500', 'backgroung-red-100', 'text-red-500', 'p-3','mt-5', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error');
