af9619fc608ed423ccd60fa462eaaf3615b204b1
PeterArce96 <parce0496@gmail.com>
2022-09-20 22:00:34 -0500

    Carrito - Mostrar en el Carrito el curso seleccionado

--- Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
+++ Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
@@ -4,6 +4,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -36,5 +37,37 @@ function leerDatosCurso(curso) {
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1,
     }
-    console.log(infoCurso);
+    // agrega elementos al arreglo del carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+// Muestra el carrito de compras en el HTML
+function carritoHTML() {
+    // Limpiar el HTML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>`
+
+        // Agregar el HTML del carrito en el body
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+    // forma Lenta
+    // contenedorCarrito.innerHTML = '';
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
 }
