4aea63301a48fd3bfd6e0984a295f1af42ab52e5
PeterArce96 <parce0496@gmail.com>
2022-09-21 17:09:48 -0500

    EnviarEmail - DValidar campos del formulario

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,9 +1,20 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
 
+// Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
 eventListeners();
 function eventListeners(){
+    // Cuando el app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 
@@ -15,5 +26,14 @@ function iniciarApp() {
     btnEnviar.disabled = true;
     // clases de Tailwind.css, cursor-not-allowed: que el puntero se convierta en un circulo rojo tachado, opacity-50: quitarle el color con opacidad
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+// Validar formulario
+function validarFormulario(e) {
+    if(e.target.value.length > 0.){
+        console.log('Si hay algo');
+    }else {
+        e.target.classList.add('border', 'border-red-500');
+    }
 }
 
