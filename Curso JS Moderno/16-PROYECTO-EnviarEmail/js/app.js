7b6c7707c74cb8a83effe99b100f4f1b6c6df2a0
PeterArce96 <parce0496@gmail.com>
2022-09-21 17:42:58 -0500

    EnviarEmail - Cambiar color del input a pasar la validación

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -32,17 +32,34 @@ function iniciarApp() {
 // Validar formulario
 function validarFormulario(e) {
     if(e.target.value.length > 0.){
-        console.log('Si hay algo');
+        // Elimina los errores...
+        const error = document.querySelector('p.error');
+        error.remove();
+
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
     }else {
+        e.target.classList.remove('border', 'border-green-500');
         e.target.classList.add('border', 'border-red-500');
 
         mostrarError('Todos los campos son obligatorios');
     }
 
     if (e.target.type === 'email') {
-        const resultado = e.target.value.indexOf('@');
-        if(resultado < 0){
-            mostrarError('El email no es válido');
+        // Expresión regular, se encuentra en internet para hacer una mejor validación
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(er.test(e.target.value)){
+            const error = document.querySelector('p.error');
+            error.remove();
+
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+        }else{
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+            
+            mostrarError('Email no válido')
         }
     }
 }
