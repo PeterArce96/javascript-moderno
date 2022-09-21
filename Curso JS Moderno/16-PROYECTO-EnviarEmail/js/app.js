89053f431e98cc4577af6012210397f54c3b0049
PeterArce96 <parce0496@gmail.com>
2022-09-21 17:53:47 -0500

    EnviarEmail - CReordenar código y habilitar botón Enviar

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -7,6 +7,9 @@ const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 
+// Expresión regular, se encuentra en internet para hacer una mejor validación
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 eventListeners();
 function eventListeners(){
     // Cuando el app arranca
@@ -34,7 +37,9 @@ function validarFormulario(e) {
     if(e.target.value.length > 0.){
         // Elimina los errores...
         const error = document.querySelector('p.error');
-        error.remove();
+        if (error) {
+            error.remove();
+        }
 
         e.target.classList.remove('border', 'border-red-500');
         e.target.classList.add('border', 'border-green-500');
@@ -46,23 +51,30 @@ function validarFormulario(e) {
     }
 
     if (e.target.type === 'email') {
-        // Expresión regular, se encuentra en internet para hacer una mejor validación
-        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+        // Validar la expresion regular
         if(er.test(e.target.value)){
             const error = document.querySelector('p.error');
-            error.remove();
+            if (error) {
+                error.remove();
+            }
 
             e.target.classList.remove('border', 'border-red-500');
             e.target.classList.add('border', 'border-green-500');
         }else{
             e.target.classList.remove('border', 'border-green-500');
             e.target.classList.add('border', 'border-red-500');
-            
+
             mostrarError('Email no válido')
         }
     }
+
+    if (er.test(email.value) && asunto.value !== '' && mensaje.value !== '') {
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
+    }
 }
+
+
 
 function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
