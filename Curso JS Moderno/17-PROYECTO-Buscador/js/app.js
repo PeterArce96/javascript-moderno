12b3f0c867293f467c74de999624b8f8819c5990
PeterArce96 <parce0496@gmail.com>
2022-09-22 22:29:25 -0500

    Buscardo - FMostrar mensaje de no hay resultados

--- Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
+++ Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -106,8 +106,23 @@ function llenarSelect() {
 // funcion que filtra en base a la búsqueda
 function filtrarAuto() {
     const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
+
     
-    mostrarAutos(resultado);
+
+    if (resultado.length) {
+        mostrarAutos(resultado);
+    }else {
+        noResultado();
+    }
+}
+
+function noResultado() {
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = ' No hay Resultados, intenta con otra búsqueda';
+    resultado.appendChild(noResultado);
 }
 
 function filtrarMarca(auto) {
