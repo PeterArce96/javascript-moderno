091e2c8adf7180fdcc036c8cb4b42bae93c335c3
PeterArce96 <parce0496@gmail.com>
2022-09-22 21:30:06 -0500

    Buscador - Primeros pasos del proyecto

---
+++ Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,23 @@
+// VARIABLES
+const resultado = document.querySelector('#resultado');
+
+// EVENTOS
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos();
+});
+
+
+// FUNCIONES
+function mostrarAutos() {
+    autos.forEach(auto => {
+        const {marca, modelo, year, puertas, transmision, precio, color} = auto;
+        const autoHTML = document.createElement('p');
+
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: ${precio} - Color: ${color}
+        `;
+
+        // insertar en el html
+        resultado.appendChild(autoHTML);
+    });
+}
