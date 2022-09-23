46ab9dc447acb1f95b419d9cd8e29f8abccbaa66
PeterArce96 <parce0496@gmail.com>
2022-09-22 21:55:48 -0500

    Buscador -  Leer valores del select y agregarlo al objeto de busqueda

--- Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
+++ Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -1,8 +1,29 @@
 // VARIABLES
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
+// Años máximo(actual) y mínimo
 const max = new Date().getFullYear();
 const min = max - 10;
+
+// Generar un objeto con la busqueda
+const datosBusqueda= {
+    marca:'',
+    year:'',
+    minimo:'',
+    maximo:'',
+    puertas:'',
+    transmision:'',
+    color:'',
+};
 
 
 // EVENTOS
@@ -13,6 +34,28 @@ document.addEventListener('DOMContentLoaded', () => {
     llenarSelect();
 });
 
+// EventListener para los select de busqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+})
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+})
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+})
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+})
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+})
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+})
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+})
 
 // FUNCIONES
 function mostrarAutos() {
