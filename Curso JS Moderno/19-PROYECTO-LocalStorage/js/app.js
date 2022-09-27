892e0782508f8da1fe72f0db7462e025ffc51d67
PeterArce96 <parce0496@gmail.com>
2022-09-26 19:15:21 -0500

    LocalStorage - Validando el formulario

--- Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -13,8 +13,33 @@ function eventListeners() {
 
 
 // FUNCIONES
+
+// Agregar tweet
 function agregarTweet(e) {
     e.preventDefault();
+    // textarea donde escribe el usuario
+    const tweet = document.querySelector('#tweet').value;
 
-    console.log('Agregando');
+    // validacion
+    if(tweet === ''){
+        mostrarError('Un mensaje no puede ir vacío');
+
+        return; //evita que se ejecuten más líneas de código
+    }
+    console.log('agregando tweet');
+}
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
