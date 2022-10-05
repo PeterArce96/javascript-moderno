cf0f75b050ff57c0232f641c0cf4c2a465b6a782
PeterArce96 <parce0496@gmail.com>
2022-10-05 18:23:47 -0500

    GastoSemanal - PCreando e instanciando la clase de Presupuesto

--- Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
+++ Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -9,18 +9,36 @@ function eventListeners() {
 }
 
 // CLASES
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI{
+
+}
+
+// Instanciar
+const ui = new UI();
+
+// Crear variable global para usarla en varias funciones o clases
+let presupuesto;
 
 // FUNCIONES
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
-    console.log(Number(presupuestoUsuario)); //convertir a numero
+    // console.log(Number(presupuestoUsuario)); //convertir a numero
 
     // validar que el presupuesto no esté vacío, cancele, envíe una letra o es número negativo
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         alert(' *Debe ingresar un presupuesto válido* ');
         window.location.reload(); //recargar página
-    } else {
-        
     }
+
+    // presupuesto válido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
 }
