8412e30d5f67a268680664c199954dcc22e9ae01
PeterArce96 <parce0496@gmail.com>
2022-10-05 23:31:44 -0500

    AdministrarCitas - Eliminar las citas

--- Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -19,6 +19,10 @@ class Citas {
         this.citas = [...this.citas, cita];
         console.log(this.citas);
     }
+
+    eliminarCita(id){
+        this.citas = this.citas.filter(cita => cita.id !== id)
+    }
 }
 
 class UI {
@@ -78,6 +82,13 @@ class UI {
             const sintomasParrafo = document.createElement('p');
             sintomasParrafo.innerHTML = `<span class="font-weight-bold">Síntomas: </span> ${sintomas}`;
 
+            // Boton para eliminar esta cita
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
+
+            btnEliminar.onclick = () => eliminarCita(id);
+
             // Agregar parrafos al divCita
             divCita.appendChild(mascotaParrafo);
             divCita.appendChild(propietarioParrafo);
@@ -85,6 +96,7 @@ class UI {
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
 
             // agregar las citas al HTML
             contenedorCitas.appendChild(divCita);
@@ -163,4 +175,16 @@ function reiniciarObjeto() {
     citaObj.fecha = '';
     citaObj.hora = '';
     citaObj.sintomas = '';
+}
+
+function eliminarCita(id) {
+    // eliminar la cita
+    administrarCitas.eliminarCita(id);
+
+    // muestre un mensaje
+    ui.imprimirAlerta('La cita se eliminó correctamente');
+
+
+    // refrescar las citas
+    ui.imprimirCitas(administrarCitas);
 }
