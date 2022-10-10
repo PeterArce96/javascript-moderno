d1cbadf718db9e3de4d496f3be8e4f5702383328
PeterArce96 <parce0496@gmail.com>
2022-10-10 17:50:53 -0500

    IndexedDB - Creando nuevo cliente mediante transacción

--- Curso JS Moderno/30-IndexedDB/js/app.js
+++ Curso JS Moderno/30-IndexedDB/js/app.js
@@ -6,8 +6,14 @@
 
 // Localstorage --> Bueno para almacenar poca información (carrito de compras o un JSON web token)
 
+let DB;
+
 document.addEventListener('DOMContentLoaded', () => {
     crmDB();
+
+    setTimeout( () => {
+        crearCliente();
+    }, 5000);
 })
 
 function crmDB() {
@@ -15,15 +21,17 @@ function crmDB() {
     let crmDB = window.indexedDB.open('crm', 1);
 
     // Si hay un error
-    crmDB.onerror = function () {
+    crmDB.onerror = function() {
         console.log('Hubo un error en la creación');
     }
     // Si se creó bien
-    crmDB.onsuccess = function () {
+    crmDB.onsuccess = function() {
         console.log('Base de datos creada');
+
+        DB = crmDB.result;
     }
     // Configuración de la base de datos, se ejecuta una sola vez
-    crmDB.onupgradeneeded = function (e) {
+    crmDB.onupgradeneeded = function(e) {
         // console.log(e.target.result);
         const db = e.target.result;
 
@@ -39,4 +47,30 @@ function crmDB() {
 
         console.log('Columnas creadas');
     }
+}
+
+function crearCliente() {
+    // DB.transaction(['base de datos'], 'modo'-> readwrite, lectura y escritura. 'readonly', solo lectura)
+    let transaction = DB.transaction(['crm'], 'readwrite');
+
+    transaction.oncomplete = function() {
+        console.log('Transacción Completada');
+    }
+
+    transaction.onerror = function() {
+        console.log('Hubo un error en la transacción');
+    }
+
+    const objectStore = transaction.objectStore('crm');
+    const nuevoCliente = {
+        telefono: 95561651,
+        nombre: 'Peter',
+        email: 'parce0496@gmail.com'
+    }
+
+    // .add --> agregar
+    // .put --> actualizar
+    // .delete --> borrar
+    const peticion = objectStore.add(nuevoCliente);
+    console.log(peticion);
 }
