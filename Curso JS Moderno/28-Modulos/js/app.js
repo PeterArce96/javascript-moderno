26d10f2b2d14913a59232ae09f35d5f27f6f55b1
PeterArce96 <parce0496@gmail.com>
2022-10-10 16:12:33 -0500

    Modulos - Pasos básicos de Módulos

---
+++ Curso JS Moderno/28-Modulos/js/app.js
@@ -0,0 +1,8 @@
+// Módulos
+
+// import --> importas la variable nombreCliente de cliente.js
+import {nombreCliente, ahorro} from './cliente.js'
+
+// trae el nombreCliente del archivo cliente.js. Esto puede traer errores al trabajar, ya que se combinan archivos
+console.log(nombreCliente);
+console.log(ahorro);
---
+++ Curso JS Moderno/28-Modulos/js/cliente.js
@@ -0,0 +1,16 @@
+
+// // Mantiene los datos localmente, privadas en este archivo
+// (function () {
+//     console.log('Desde un IIFE');
+
+//     // Para que archivos o variables no se combinen con otro archivo, se coloca dentro de in IIFE
+//     // const nombreCliente = 'Peter';
+
+//     // Si lo haces con window, si es leído por los otros archivos
+//     window.nombreCliente = 'Juan';
+// })();
+
+// export --> Exportar una variable desde un archivo.
+export const nombreCliente = 'Juan';
+
+export const ahorro = 200;
