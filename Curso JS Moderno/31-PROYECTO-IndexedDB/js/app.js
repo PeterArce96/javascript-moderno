fcc16d457c66ff81bfc2b36a3ac9f0532c7f390b
PeterArce96 <parce0496@gmail.com>
2022-10-10 18:07:21 -0500

    IndexedDBCitas - Agregando una cita a la BD

--- Curso JS Moderno/31-PROYECTO-IndexedDB/js/app.js
+++ Curso JS Moderno/31-PROYECTO-IndexedDB/js/app.js
@@ -203,7 +203,7 @@ function nuevaCita(e) {
         editando = false;
 
     } else {
-        // Nuevo Registrando
+        // Nuevo Registro
 
         // Generar un ID único
         citaObj.id = Date.now();
@@ -211,8 +211,21 @@ function nuevaCita(e) {
         // Añade la nueva cita
         administrarCitas.agregarCita({...citaObj});
 
-        // Mostrar mensaje de que todo esta bien...
-        ui.imprimirAlerta('Se agregó correctamente')
+        // Insertar registro en IndexedDB
+        const transaction = DB.transaction(['citas'], 'readwrite');
+        // Habilitar el objectStore
+        const objectStore = transaction.objectStore('citas');
+        // Insertar en la base de datos
+        objectStore.add(citaObj);
+
+        transaction.oncomplete = function() {
+            console.log('Cita agregada');
+
+            // Mostrar mensaje de que todo esta bien...
+            ui.imprimirAlerta('Se agregó correctamente')
+        }
+
+        
     }
 
 
