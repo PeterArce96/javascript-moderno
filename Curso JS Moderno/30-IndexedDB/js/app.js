afdb8df58d0eaf888d826af137bc05310180e595
PeterArce96 <parce0496@gmail.com>
2022-10-10 17:22:51 -0500

    IndexedDB - Teoría

---
+++ Curso JS Moderno/30-IndexedDB/js/app.js
@@ -0,0 +1,8 @@
+// IndexedDB
+// Almacenar grandes cantidades de información
+// Base de datos completa, pero estos datos son visibles por lo que no es recomendable almacenar passwords o tarjetas de crédito.
+// Mientras que en localstorage solo se almacenan Strings, aquí se puede almacenar strings, booleans, archivos de cualquier tipo soportado por JS
+// No tiene 'limites' pero cuando hay un archivo de 50mb o + , requieres permisos
+
+// Localstorage 
+// Bueno para almacenar poca información (carrito de compras o un JSON web token)
