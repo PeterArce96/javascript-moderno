e8aa814518b18b15e9988633fb193c0bce480794
PeterArce96 <parce0496@gmail.com>
2022-10-18 22:55:28 -0500

    APIs - DSpeech API, voz

---
+++ Curso JS Moderno/34-APIs-JS/js/06-app.js
@@ -0,0 +1,42 @@
+// APIs - Speech API
+
+const salida = document.querySelector('#salida');
+const microfono = document.querySelector('#microfono');
+
+microfono.addEventListener('click', ejecutarSpeechAPI);
+
+function ejecutarSpeechAPI() {
+    const SpeechRecognition = webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    recognition.start();
+
+    recognition.onstart = function() {
+        salida.classList.add('mostrar');
+        salida.textContent = 'Escuchando...';
+    };
+
+    recognition.onspeechend = function() {
+        salida.textContent = 'Se dejó de grabar...';
+        recognition.stop();
+    };
+
+    recognition.onresult = function(e) {
+        console.log(e.results);
+        console.log(e.results[0][0]);
+
+        const {confidence, transcript} = e.results[0][0];
+
+        const speech = document.createElement('p');
+        speech.innerHTML = `Grabado: ${transcript}`;
+
+        const seguridad = document.createElement('p');
+        seguridad.innerHTML = `Seguridad: ${parseInt( confidence * 100 )}`;
+
+        salida.appendChild(speech);
+        salida.appendChild(seguridad);
+    };
+}
+// SpeechRecognitionResult muestra 2 resultados
+// confidence: nivel de confianza de detectar lo que se habló correctamente
+// transcript: escritura de lo que se dijo por voz.
