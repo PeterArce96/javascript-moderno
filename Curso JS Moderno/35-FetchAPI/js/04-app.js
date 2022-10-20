e2eeaefb49ca639b44e658d2f202c433f7fd15b8
PeterArce96 <parce0496@gmail.com>
2022-10-19 22:26:30 -0500

    FetchAPI - Consultar e Imprimir los resultados de una API

---
+++ Curso JS Moderno/35-FetchAPI/js/04-app.js
@@ -0,0 +1,32 @@
+// FetchAPI - Consultar e imprimir los resultados de un API
+
+const cargarAPIBtn = document.querySelector('#cargarAPI');
+cargarAPIBtn.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+    const url = 'https://picsum.photos/list';
+
+    fetch(url)
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(resultado => {
+            mostrarHTML(resultado);
+        })
+}
+
+function mostrarHTML(datos) {
+    const contenido = document.querySelector('.contenido');
+
+    let html = '';
+
+    datos.forEach(perfil => {
+        const {author, post_url} = perfil;
+
+        html += `
+        <p>Autor: ${author}</p>
+        <a href="${post_url}" target="_blank">Ver Imagen</a>`
+    });
+
+    contenido.innerHTML = html;
+}
