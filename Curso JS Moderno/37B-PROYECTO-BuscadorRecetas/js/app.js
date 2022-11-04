dabc264e73dc993adaa0949d5dfb542e2f313160
PeterArce96 <parce0496@gmail.com>
2022-11-03 22:02:03 -0500

    RecetasAPI - OMostrando información en el Modal

--- Curso JS Moderno/37B-PROYECTO-BuscadorRecetas/js/app.js
+++ Curso JS Moderno/37B-PROYECTO-BuscadorRecetas/js/app.js
@@ -4,6 +4,7 @@ function iniciarApp() {
     selectCategorias.addEventListener('change', seleccionarCategoria);
 
     const resultado = document.querySelector('#resultado');
+    const modal = new bootstrap.Modal('#modal', {});
 
     obtenerCategorias();
 
@@ -97,7 +98,22 @@ function iniciarApp() {
     }
 
     function mostrarRecetaModal(receta) {
-        
+
+        const {idMeal, strInstructions, strMeal, strMealThumb} = receta;
+
+        // Añadir contenido al modal
+        const modalTitle = document.querySelector('.modal .modal-title');
+        const modalBody = document.querySelector('.modal .modal-body');
+
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+        `;
+
+        // Muestra el modal
+        modal.show();
     }
 
     function limpiarHTML(selector) {
