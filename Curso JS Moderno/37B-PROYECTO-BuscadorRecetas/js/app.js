19ca725b8b5e0a0ca440c6d26e23295591a9b040
PeterArce96 <parce0496@gmail.com>
2022-11-03 21:49:15 -0500

    RecetasAPI - Obtener receta que se mostrará en el modal

--- Curso JS Moderno/37B-PROYECTO-BuscadorRecetas/js/app.js
+++ Curso JS Moderno/37B-PROYECTO-BuscadorRecetas/js/app.js
@@ -91,6 +91,12 @@ function iniciarApp() {
 
     function seleccionarReceta(id) {
         const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarRecetaModal(resultado.meals[0]))
+    }
+
+    function mostrarRecetaModal(receta) {
         
     }
 
