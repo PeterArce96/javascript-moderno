f26e3554668afbf202377e780a8480418dea865e
PeterArce96 <parce0496@gmail.com>
2022-11-15 22:31:42 -0500

    Pixabay - MNavegar por las páginas

--- Curso JS Moderno/38-PROYECTO-PixabayImagenes/js/app.js
+++ Curso JS Moderno/38-PROYECTO-PixabayImagenes/js/app.js
@@ -6,6 +6,7 @@ const paginacionDiv = document.querySelector('#paginacion');
 const registroPorPagina = 40;
 let totalPaginas;
 let iterador;
+let paginaActual = 1;
 
 window.onload = () => {
     formulario.addEventListener('submit', validarFormulario);
@@ -21,7 +22,7 @@ function validarFormulario(e) {
         return;
     }
 
-    buscarImagenes(terminoBusqueda);
+    buscarImagenes();
 }
 
 function mostrarAlerta(mensaje) {
@@ -46,9 +47,10 @@ function mostrarAlerta(mensaje) {
     
 }
 
-function buscarImagenes(termino) {
+function buscarImagenes() {
+    const termino = document.querySelector('#termino').value;
     const key = '31358842-01d0c7914f282cdfdf6eae111';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registroPorPagina}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registroPorPagina}&page=${paginaActual}`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -119,7 +121,12 @@ function imprimirPaginador() {
         boton.href = '#';
         boton.dataset.pagina = value;
         boton.textContent = value;
-        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'rounded');
+
+        boton.onclick = () => {
+            paginaActual = value;
+            buscarImagenes();
+        }
 
         paginacionDiv.appendChild(boton);
     }
