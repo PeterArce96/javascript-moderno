df1f3fa18fb46bacb3fd7166ecfee21124884802
PeterArce96 <parce0496@gmail.com>
2022-11-16 23:23:02 -0500

    Criptomonedas -  Consultando la API

--- Curso JS Moderno/39-PROYECTO-Criptomonedas/js/app.js
+++ Curso JS Moderno/39-PROYECTO-Criptomonedas/js/app.js
@@ -56,6 +56,7 @@ function submitFormulario(e) {
     }
 
     // Consultar la API con los resultados
+    consultarAPI();
 }
 
 function mostrarAlerta(msg) {
@@ -75,4 +76,19 @@ function mostrarAlerta(msg) {
         }, 3000);
     }
     
+}
+
+function consultarAPI() {
+    const {moneda, criptomoneda} = objBusqueda;
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(cotizacion => {
+            mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+        })
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+    console.log(cotizacion);
 }
