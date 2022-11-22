1540acdd40e7ab36d2197660ddaf5f6fd6d8c2ef
PeterArce96 <parce0496@gmail.com>
2022-11-22 17:37:40 -0500

    Propinas - Limpiar el resumen al actualizar pedido

--- Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -140,6 +140,9 @@ function agregarPlatillo(producto) {
         cliente.pedido = [...resultado];
     }
 
+    // Limpiar el codigo HTML previo
+    limpiarHTML();
+
     // Mostrar el resumen
     actualizarResumen();
 }
@@ -148,7 +151,7 @@ function actualizarResumen() {
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('DIV');
-    resumen.classList.add('col-md-6');
+    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
 
     // Información de la MESA
     const mesa = document.createElement('P');
@@ -172,7 +175,25 @@ function actualizarResumen() {
     mesa.appendChild(mesaSpan);
     hora.appendChild(horaSpan);
 
-    // Agregar al contenido
-    contenido.appendChild(mesa);
-    contenido.appendChild(hora);
+    // Título de la sección
+    const heading = document.createElement('H3');
+    heading.textContent = 'Platillos Consumidos';
+    heading.classList.add('my-4', 'text-center');
+
+    // Iterar sobre el array de pedidos
+
+    // Agregar al resumen
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora);
+    resumen.appendChild(heading);
+
+    contenido.appendChild(resumen);
+}
+
+function limpiarHTML() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    while (contenido.firstChild) {
+        contenido.removeChild(contenido.firstChild);
+    }
 }
