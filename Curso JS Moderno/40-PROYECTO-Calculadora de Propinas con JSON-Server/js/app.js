6d2fb283fe74526a41ad94d0943c8fea6b8a27cb
PeterArce96 <parce0496@gmail.com>
2022-11-22 17:52:03 -0500

    Propinas - Mostrando el pedido en el resumen

--- Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -181,11 +181,57 @@ function actualizarResumen() {
     heading.classList.add('my-4', 'text-center');
 
     // Iterar sobre el array de pedidos
+    const grupo = document.createElement('UL');
+    grupo.classList.add('list-group');
+
+    const {pedido} = cliente;
+    pedido.forEach(articulo => {
+        const {nombre, cantidad, precio, id} = articulo;
+
+        const lista = document.createElement('LI');
+        lista.classList.add('list-group-item');
+
+        // nombre del articulo
+        const nombreEl = document.createElement('H4');
+        nombreEl.classList.add('my-4');
+        nombreEl.textContent = nombre;
+
+        // cantidad del articulo
+        const cantidadEl = document.createElement('P');
+        cantidadEl.classList.add('fw-bold');
+        cantidadEl.textContent = 'Cantidad : ';
+
+        const cantidadValor = document.createElement('SPAN');
+        cantidadValor.classList.add('fw-normal');
+        cantidadValor.textContent = cantidad;
+
+        // precio del articulo
+        const precioEl = document.createElement('P');
+        precioEl.classList.add('fw-bold');
+        precioEl.textContent = `Precio: `;
+
+        const precioValor = document.createElement('SPAN');
+        precioValor.classList.add('fw-normal');
+        precioValor.textContent = `$${precio}`;
+
+        // Agregar valores a sus contenedores
+        cantidadEl.appendChild(cantidadValor)
+        precioEl.appendChild(precioValor)
+
+        // Agregar elementos al li
+        lista.appendChild(nombreEl);
+        lista.appendChild(cantidadEl);
+        lista.appendChild(precioEl);
+
+        // Agregar lista al grupo principal
+        grupo.appendChild(lista);
+    })
 
     // Agregar al resumen
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
     resumen.appendChild(heading);
+    resumen.appendChild(grupo);
 
     contenido.appendChild(resumen);
 }
