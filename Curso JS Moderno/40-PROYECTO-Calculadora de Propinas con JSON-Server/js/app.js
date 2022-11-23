32ab47244289dc648ea343029efd536d5becd043
PeterArce96 <parce0496@gmail.com>
2022-11-22 22:17:14 -0500

    Propinas - Calculando propinas

--- Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -327,6 +327,7 @@ function formularioPropinas() {
     radio10.name = 'propina';
     radio10.value = '10';
     radio10.classList.add('form-check-input');
+    radio10.onclick = calcularPropina;
 
     const radio10Label = document.createElement('LABEL');
     radio10Label.textContent = '10%';
@@ -344,6 +345,8 @@ function formularioPropinas() {
     radio25.name = 'propina';
     radio25.value = '25';
     radio25.classList.add('form-check-input');
+    radio25.onclick = calcularPropina;
+
 
     const radio25Label = document.createElement('LABEL');
     radio25Label.textContent = '25%';
@@ -361,6 +364,8 @@ function formularioPropinas() {
     radio50.name = 'propina';
     radio50.value = '50';
     radio50.classList.add('form-check-input');
+    radio50.onclick = calcularPropina;
+
 
     const radio50Label = document.createElement('LABEL');
     radio50Label.textContent = '50%';
@@ -382,4 +387,26 @@ function formularioPropinas() {
     formulario.appendChild(divFormulario);
 
     contenido.appendChild(formulario);
+}
+
+function calcularPropina() {
+    const {pedido} = cliente;
+    let subtotal = 0;
+
+    // Calcular subtotal a pagar
+    pedido.forEach(articulo => {
+        subtotal += articulo.cantidad * articulo.precio;
+    })
+
+    // Seleccionar radiobutton con propina del cliente
+    const propinaSeleccionada = document.querySelector('[name="propina"]:checked').value;
+
+    // Calcular la propina
+    const propina = ((subtotal * parseInt(propinaSeleccionada)) / 100);
+    console.log(propina);
+
+    // Calcular el total a pagar
+    const total = subtotal + propina;
+
+    console.log(total);
 }
