89f1ac932a56769920942faf929b3aa2127d2732
PeterArce96 <parce0496@gmail.com>
2022-12-13 21:03:41 -0500

    DesignPatterns - Que son los Design Patterns?

---
+++ Curso JS Moderno/48-DesignPatterns/js/01-app.js
@@ -0,0 +1,8 @@
+// Patrones de diseño
+// 1.- Soluciones tipicas a problemas comunes en Desarrollo de software, cada patrón
+// es como un plano que se puede personalizar para resolver un problema de diseño en el
+// código
+
+// De creación - permiten crear objetos y permiten la reutilización del código
+// De estructura - explican como deben comunicarse los objetos y clases en grandes proyectos
+// De comportamiento - se encargan de como se comportan y comunican los objetos
