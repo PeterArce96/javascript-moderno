235ecc069893c1fd17c6a6de8f89d9501f300396
PeterArce96 <parce0496@gmail.com>
2022-12-13 21:49:53 -0500

    DesignPatterns - Mixin Pattern

--- Curso JS Moderno/48-DesignPatterns/js/06-app.js
+++ Curso JS Moderno/48-DesignPatterns/js/06-app.js
@@ -1,2 +1,42 @@
 // Usado para el Module pattern
 // console.log(modulo1.nombre);
+
+// MIXIN PATTERN
+class Persona {
+    constructor(nombre, email){
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+const funcionesPersona = {
+    mostrarInformacion(){
+        console.log(`Nombre Persona: ${this.nombre}, Email: ${this.email}`);
+    },
+    mostrarNombre(){
+        console.log(`Mi nombre es: ${this.nombre}`);
+    }
+}
+
+// Añadir funcionesPersona a la clase de Persona
+Object.assign(Persona.prototype, funcionesPersona);
+
+const cliente = new Persona('Juan', 'correo@correo.com')
+console.log(cliente);
+
+cliente.mostrarInformacion();
+cliente.mostrarNombre();
+
+// Podemos crear otra clase y asignarle tambien funcionesPersona a su prototype, para poder usarlas
+class Cliente {
+    constructor(nombre, email){
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+Object.assign(Cliente.prototype, funcionesPersona);
+
+const cliente2 = new Cliente('Cliente', 'cliente@correo.com');
+console.log(cliente2);
+cliente2.mostrarInformacion();
+cliente2.mostrarNombre();
