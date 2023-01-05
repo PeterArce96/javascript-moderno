4f255d11ba85f7dac940d6e0bb18ec676869c9cc
PeterArce96 <parce0496@gmail.com>
2023-01-04 21:45:10 -0500

    JEST - Testing al proyecto de Citas - Actualizar y Eliminar

--- Curso JS Moderno/51-Testing-Jest/__tests__/citas.js
+++ Curso JS Moderno/51-Testing-Jest/__tests__/citas.js
@@ -3,8 +3,11 @@ import Citas from "../js/classes/Citas";
 describe('Probar la clase de Citas', () => {
     const citas = new Citas();
 
+    const id = Date.now()
+
     test('Agregar una nueva cita', () => {
         const citaObj = {
+            id,
             mascota: 'Hook',
             propietario: 'Juan',
             telefono: '19030913',
@@ -13,11 +16,30 @@ describe('Probar la clase de Citas', () => {
             sintomas: 'Solo duerme'
         };
 
-        citaObj.id = Date.now();
-
         citas.agregarCita(citaObj);
 
         // Prueba
+        expect(citas).toMatchSnapshot();
+    });
+
+    test('Actualizar cita', () => {
+        const citaActualizada = {
+            id,
+            mascota: 'Clifford',
+            propietario: 'Juan',
+            telefono: '19030913',
+            fecha: '10-12-2020',
+            hora:'10:30',
+            sintomas: 'Solo duerme'
+        };
+
+        citas.editarCita(citaActualizada);
+        expect(citas).toMatchSnapshot();
+    });
+
+    test('Eliminar Cita', () => {
+        citas.eliminarCita(id);
+
         expect(citas).toMatchSnapshot();
     });
 });
